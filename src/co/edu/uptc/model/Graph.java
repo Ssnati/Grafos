fad3943bfe9e02774a78b34584244809a097f0bb
Ssolaris <santieltroll.2003@gmail.com>
2023-04-12 17:32:02 -0500

    Se añade la clase grafo

--- src/co/edu/uptc/model/Graph.java
+++ src/co/edu/uptc/model/Graph.java
@@ -1,4 +1,32 @@
 package co.edu.uptc.model;
 
+import co.edu.uptc.pojo.Edge;
+import co.edu.uptc.pojo.Node;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
+    private Map<Node, List<Edge>> grafo = new HashMap<>();
+
+    public void addNode(Node Node) {
+        grafo.put(Node, new ArrayList<>());
+    }
+
+    public void addEdge(Node origin, Node destine, double distance, double speed, String wayType) {
+        Edge Edge = new Edge(origin, destine, distance, speed, wayType);
+        grafo.get(origin).add(Edge);
+        grafo.get(destine).add(Edge);
+    }
+
+    public List<Edge> getEdges(Node Node) {
+        return grafo.get(Node);
+    }
+
+    public List<Node> getNodes() {
+        return new ArrayList<>(grafo.keySet());
+    }
+
 }
